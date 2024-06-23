fef26fe7b42a19a3641dcc9c89aed2268e314367
Ashwin Shukla <ashwin72shukla@gmail.com>
2024-06-23 05:40:08 +0530

    refactor: :art:

--- blog-service/src/app.tsx
+++ blog-service/src/app.tsx
@@ -22,9 +22,4 @@ app.get('/blogs', async (c) => {
   }
 })
 
-console.log(`Server started at PORT: ${Bun.env.PORT}`)
-
-Bun.serve({
-  fetch: app.fetch,
-  port: Bun.env.PORT || 3031
-})
+export default app
