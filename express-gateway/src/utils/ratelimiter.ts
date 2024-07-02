c8f1d8ef350cc4c8ee764062eb276074578882d2
Ashwin Shukla <ashwin72shukla@gmail.com>
2024-07-02 14:39:30 +0530

    Adding biome for formatting and linting code

--- express-gateway/src/utils/ratelimiter.ts
+++ express-gateway/src/utils/ratelimiter.ts
@@ -1,11 +1,11 @@
-import rateLimit from "express-rate-limit";
+import rateLimit from 'express-rate-limit';
 
 // Adding a rate-limiter
 export const defaultRateLimiter = rateLimit({
-  windowMs: 20 * 1000, // 10 seconds
-  limit: 10,
-  validate: {
+	windowMs: 20 * 1000, // 10 seconds
+	limit: 10,
+	validate: {
 		xForwardedForHeader: false,
 		default: true,
 	},
-})
+});
